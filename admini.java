83a812c9f2f8c1ddf27158c427a96d9ba811cf13
Przekoxdowny2115 <89962787+NorbertModzelewski@users.noreply.github.com>
2021-11-26 13:31:12 +0000

    Update admini.java

--- admini.java
+++ admini.java
@@ -1,6 +1,6 @@
 public class admini extends Person {
-    public int nr_identfikatoru;
-    public char kontakt;
+    private int nr_identfikatoru;
+    private char kontakt;
 
     public admini(String imie, String nazwisko, int wiek, String adres, int kod_pocztowy, int nr_identfikatoru, char kontakt) {
         super(imie, nazwisko, wiek, adres, kod_pocztowy);
