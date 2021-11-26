ea9db1b954dcb2ad9cdb8f78449325f773891844
Przekoxdowny2115 <89962787+NorbertModzelewski@users.noreply.github.com>
2021-11-26 13:30:59 +0000

    Update Klienci.java

--- Klienci.java
+++ Klienci.java
@@ -1,6 +1,6 @@
 public class Klienci extends Person{
-    public String email;
-    public String haslo;
+    private String email;
+    private String haslo;
 
     public Klienci(String imie, String nazwisko, int wiek, String adres, int kod_pocztowy, String email, String haslo) {
         super(imie, nazwisko, wiek, adres, kod_pocztowy);
