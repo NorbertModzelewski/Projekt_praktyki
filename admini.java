e47c672c14c60245b673bc4b8300ad4a0a96328c
JD <57227707+DomikIAm@users.noreply.github.com>
2021-11-30 19:37:51 +0100

    Update admini.java

--- admini.java
+++ admini.java
@@ -2,9 +2,5 @@ public class admini extends Person {
     private int nr_identfikatoru;
     private char kontakt;
 
-    public admini(String imie, String nazwisko, int wiek, String adres, int kod_pocztowy, int nr_identfikatoru, char kontakt) {
-        super(imie, nazwisko, wiek, adres, kod_pocztowy);
-        this.nr_identfikatoru = nr_identfikatoru;
-        this.kontakt = kontakt;
-    }
+
 }
