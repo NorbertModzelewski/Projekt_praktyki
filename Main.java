1bc07b62cc0003c218bc28f009aefafb8da8dca9
JD <57227707+DomikIAm@users.noreply.github.com>
2021-12-02 19:47:09 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,8 +7,6 @@ public class Main {
         System.out.println("1. Zaloguj się");
         System.out.println("2. Zarejstuj się");
         login();
-        strona_glowna();
-
     }
 
     /**
@@ -26,10 +24,12 @@ public class Main {
             switch (wybierz) {
                 case 1:
                     Klienci.logowanie();
+                    strona_glowna();
                     break;
 
                 case 2:
                     Person.rejestracja();
+                    strona_glowna();
                     break;
                 default:
                     System.out.println("Wybierz liczbe z przedziału od 1 do 2!");
@@ -46,7 +46,7 @@ public class Main {
     }
 
     /**
-     * Funkcja umożliwiająca wybór opcji zakupu
+     * Metoda umożliwiająca wybór opcji zakupu lub sprawdzenia trwających promocji itp.
      *
      * @autorzy Norbert Modzelewski, Dominik Krucha
      */
@@ -58,13 +58,13 @@ public class Main {
 
 
                 Scanner scanner = new Scanner(System.in);
-                System.out.println("Witamy w naszym sklepie gamingowym Gaming Room");
+                System.out.println("Witamy w naszym sklepie gamingowym Gaming Room!");
                 System.out.println("1. Promocje");
                 System.out.println("2. Sprzedaż ");
                 System.out.println("3. Black Friday");
                 System.out.println("4. Rekomendowane produkty");
-                System.out.println("5. Wyjście ze strony");
-                System.out.println("6. Przejdź do koszyka");
+                System.out.println("5. Przejście do koszyka");
+                System.out.println("6. Wyjście ze strony");
                 opcje = scanner.nextInt();
                 switch (opcje) {
                     case 1:
@@ -81,13 +81,13 @@ public class Main {
                         break;
 
                     case 5:
-
+                        wyjdz=1;
+                        Koszyk.koszyk();
                         break;
 
                     case 6:
-
-                        wyjdz =1;
-                        Koszyk.koszyk();
+                        System.out.println("Do zobaczenia.");
+                        System.exit(0);
                         break;
 
                     default:
@@ -103,21 +103,25 @@ public class Main {
 
     }
 
+    /**
+     * Funkcja informująca użytkownika o aktualnych przecenach w sklepie
+     * @autorzy Norbert Modzelewski, Dominik Krucha
+     */
 
 
 
     public static void promocje(){
 
         System.out.println("Promocje do -70%");
-        System.out.println("Promocje sprzetu komputerowego");
-        System.out.println("Promocje krzeseł i biurek");
-        System.out.println("Promocje świateł led");
+        System.out.println("Promocje sprzetu komputerowego 30%");
+        System.out.println("Promocje krzeseł i biurek 40%");
+        System.out.println("Promocje świateł led 10%(GAMING)");
         strona_glowna();
     }
 
     /**
      * Funkcja umożliwiająca wybór kategori  przedmiotu który chce sie kupić
-     * @autorzy Norbert Modzelewski Krucha Dominik
+     * @autorzy Norbert Modzelewski, Dominik Krucha
      */
 
 
@@ -149,22 +153,22 @@ public class Main {
                     switch (opcje) {
                         case 1:
                             System.out.println("Ultra Gaming");
-                            System.out.println("Kupiłeś komputer Ultra Gaming.");
+                            System.out.println("Dodałeś do koszyka komputer Ultra Gaming.");
                             Koszyk.koszyk.add("Komputer ultra gaming");
                             sprzedaz();
 
                             break;
                         case 2:
                             System.out.println("Ultra Gaming Master");
-                            System.out.println("Kupiłeś komputer Ultra Gaming Master.");
-                            Koszyk.koszyk.add("komputer Ultra Gaming Master");
+                            System.out.println("Dodałeś do koszyka komputer Ultra Gaming Master.");
+                            Koszyk.koszyk.add("Komputer Ultra Gaming Master");
                             sprzedaz();
 
                             break;
                         case 3:
                             System.out.println("Champion");
-                            System.out.println("Kupiłeś komputer Champion.");
-                            Koszyk.koszyk.add("komputer komputer Champion");
+                            System.out.println("Dodałeś do koszyka komputer Champion.");
+                            Koszyk.koszyk.add("Komputer komputer Champion");
                             sprzedaz();
 
                             break;
@@ -178,21 +182,21 @@ public class Main {
                     switch (opcje) {
                         case 1:
                             System.out.println("Razer Viper");
-                            System.out.println("Kupiłeś myszke Razer Viper.");
-                            Koszyk.koszyk.add("myszka Razer Viper");
+                            System.out.println("Dodałeś do koszyka myszke Razer Viper.");
+                            Koszyk.koszyk.add("Myszka Razer Viper");
                             sprzedaz();
 
                             break;
                         case 2:
                             System.out.println("Endgame Gear XM1");
-                            System.out.println("Kupiłeś myszke Endgame Gear XM1.");
-                            Koszyk.koszyk.add("myszka Endgame Gear XM1");
+                            System.out.println("Dodałeś do koszyka myszke Endgame Gear XM1.");
+                            Koszyk.koszyk.add("Myszka Endgame Gear XM1");
                             sprzedaz();
 
                             break;
                         case 3:
                             System.out.println("Corsair Sabre RGB Pro");
-                            System.out.println("Kupiłeś myszke Corsair Sabre RGB Pro.");
+                            System.out.println("Dodałeś do koszyka myszke Corsair Sabre RGB Pro.");
                             Koszyk.koszyk.add("myszka Corsair Sabre RGB Pro");
                             sprzedaz();
 
@@ -206,21 +210,21 @@ public class Main {
                     switch (opcje) {
                         case 1:
                             System.out.println("Asus ROG Strix Scope");
-                            System.out.println("Kupiłeś klawiature Asus ROG Strix Scope.");
+                            System.out.println("Dodałeś do koszyka klawiature Asus ROG Strix Scope.");
                             Koszyk.koszyk.add("klawiatura Asus ROG Strix Scope");
                             sprzedaz();
 
                             break;
                         case 2:
                             System.out.println("Cherry G80-3000");
-                            System.out.println("Kupiłeś klawiature Cherry G80-3000.");
+                            System.out.println("Dodałeś do koszyka klawiature Cherry G80-3000.");
                             Koszyk.koszyk.add("klawiatura Cherry G80-3000");
                             sprzedaz();
 
                             break;
                         case 3:
                             System.out.println("Corsair Gaming K95 RGB");
-                            System.out.println("Kupiłeś klawiature Corsair Gaming K95 RGB.");
+                            System.out.println("Dodałeś do koszyka klawiature Corsair Gaming K95 RGB.");
                             Koszyk.koszyk.add("klawiatura Corsair Gaming K95 RGB");
                             sprzedaz();
 
@@ -234,21 +238,21 @@ public class Main {
                     switch (opcje) {
                         case 1:
                             System.out.println("X");
-                            System.out.println("Kupiłeś podstawke X.");
+                            System.out.println("Dodałeś do koszyka podstawke X.");
                             Koszyk.koszyk.add("Podstwaka X");
                             sprzedaz();
 
                             break;
                         case 2:
                             System.out.println("Y");
-                            System.out.println("Kupiłeś podstwke Y.");
+                            System.out.println("Dodałeś do koszyka podstwke Y.");
                             Koszyk.koszyk.add("Podstwaka Y");
                             sprzedaz();
 
                             break;
                         case 3:
                             System.out.println("Z");
-                            System.out.println("Kupiłeś podstawke Z.");
+                            System.out.println("Dodałeś do koszyka podstawke Z.");
                             Koszyk.koszyk.add("Podstwaka Z");
                             sprzedaz();
 
@@ -262,21 +266,21 @@ public class Main {
                     switch (opcje) {
                         case 1:
                             System.out.println("ASUS VG248QG");
-                            System.out.println("Kupiłeś monitor ASUS VG248QG.");
+                            System.out.println("Dodałeś do koszyka monitor ASUS VG248QG.");
                             Koszyk.koszyk.add("Monitor ASUS VG248QG");
                             sprzedaz();
 
                             break;
                         case 2:
                             System.out.println("Iiyama ProLite XUB2796QSU-B1");
-                            System.out.println("Kupiłeś monitor Iiyama ProLite XUB2796QSU-B1.");
+                            System.out.println("Dodałeś do koszyka monitor Iiyama ProLite XUB2796QSU-B1.");
                             Koszyk.koszyk.add("Monitor Iiyama ProLite XUB2796QSU-B1");
                             sprzedaz();
 
                             break;
                         case 3:
                             System.out.println("LG 27GN880 Ergo ");
-                            System.out.println("Kupiłeś monitor LG 27GN880 Ergo.");
+                            System.out.println("Dodałeś do koszyka monitor LG 27GN880 Ergo.");
                             Koszyk.koszyk.add("Monitor LG 27GN880 Ergo");
                             sprzedaz();
 
@@ -290,21 +294,21 @@ public class Main {
                     switch (opcje) {
                         case 1:
                             System.out.println("PSC 20zł");
-                            System.out.println("Kupiłeś PSC za 20zł.");
+                            System.out.println("Dodałeś do koszyka PSC za 20zł.");
                             Koszyk.koszyk.add("PSC 20 zł");
                             sprzedaz();
 
                             break;
                         case 2:
                             System.out.println("PSC 50zł");
-                            System.out.println("Kupiłeś PSC za 50zł.");
+                            System.out.println("Dodałeś do koszyka PSC za 50zł.");
                             Koszyk.koszyk.add("PSC 50 zł");
                             sprzedaz();
 
                             break;
                         case 3:
                             System.out.println("PSC 100zł");
-                            System.out.println("Kupiłeś PSC za 100zł.");
+                            System.out.println("Dodałeś do koszyka PSC za 100zł.");
                             Koszyk.koszyk.add("PSC 100 zł");
                             sprzedaz();
 
@@ -318,21 +322,21 @@ public class Main {
                     switch (opcje) {
                         case 1:
                             System.out.println("Diablo Chairs X-ONE");
-                            System.out.println("Kupiłeś krzesło Diablo Chairs X-ONE.");
+                            System.out.println("Dodałeś do koszyka krzesło Diablo Chairs X-ONE.");
                             Koszyk.koszyk.add("Krzesło Diablo Chairs X-ONE");
                             sprzedaz();
 
                             break;
                         case 2:
                             System.out.println("MAD DOG GCH700");
-                            System.out.println("Kupiłeś krzesło MAD DOG GCH700.");
+                            System.out.println("Dodałeś do koszyka krzesło MAD DOG GCH700.");
                             Koszyk.koszyk.add("Krzesło MAD DOG GCH700");
                             sprzedaz();
 
                             break;
                         case 3:
                             System.out.println("Kraken");
-                            System.out.println("Kupiłeś krzesło Kraken.");
+                            System.out.println("Dodałeś do koszyka krzesło Kraken.");
                             Koszyk.koszyk.add("Krzesło Kraken");
                             sprzedaz();
 
@@ -346,21 +350,21 @@ public class Main {
                     switch (opcje) {
                         case 1:
                             System.out.println("Ultradesk SPACE XXL");
-                            System.out.println("Kupiłeś biurko Ultradesk SPACE XXL.");
+                            System.out.println("Dodałeś do koszyka biurko Ultradesk SPACE XXL.");
                             Koszyk.koszyk.add("Biurko Ultradesk SPACE XXL");
                             sprzedaz();
 
                             break;
                         case 2:
                             System.out.println("‎Silver Monkey");
-                            System.out.println("Kupiłeś biurko ‎Silver Monkey.");
+                            System.out.println("Dodałeś do koszyka biurko ‎Silver Monkey.");
                             Koszyk.koszyk.add("Biurko ‎Silver Monkey");
                             sprzedaz();
 
                             break;
                         case 3:
                             System.out.println("Huzaro Hero 5.0 White");
-                            System.out.println("Kupiłeś biurko Huzaro Hero 5.0 White.");
+                            System.out.println("Dodałeś do koszyka biurko Huzaro Hero 5.0 White.");
                             Koszyk.koszyk.add("Biurko Huzaro Hero 5.0 White");
                             sprzedaz();
 
@@ -368,10 +372,8 @@ public class Main {
 
                     }
                 case 9:
-                    for (int i = 0; i < Koszyk.koszyk.size(); i++) {
-                        System.out.println(Koszyk.koszyk.get(i));
-                    }
                     strona_glowna();
+
                     break;
 
 
@@ -397,14 +399,12 @@ public class Main {
         System.out.println("W piatek 26.11.2021 będa promocje na większość przedmiotów w naszym sklepie");
         strona_glowna();
     }
+
+
     /**
      * Funkcja pokazująca rekomendowane produkty i umożliwiająca zakup sprzętu komputerowego
-     * @autorzy Norbert modzelewski, Dominik Krucha
+     * @autorzy Norbert Modzelewski, Dominik Krucha
      */
-
-
-
-
     public static void rekomendowane_produkty() {
         int opcje;
         Scanner scanner = new Scanner(System.in);
@@ -419,22 +419,22 @@ public class Main {
                     opcje = scanner.nextInt();
                     switch (opcje) {
                         case 1:
-                            System.out.println("Myszka Genesis");
-                            System.out.println("Kupiłeś myszke Genesis.");
+                            System.out.println("Myszka Genesis Krypton110");
+                            System.out.println("Dodałeś do koszyka myszke Genesis.");
                             Koszyk.koszyk.add("Myszka Genesis");
                             rekomendowane_produkty();
 
                             break;
                         case 2:
-                            System.out.println("Myszka SteelSeries");
-                            System.out.println("Kupiłeś myszke SteelSeries.");
+                            System.out.println("Myszka SteelSeries Rival 3");
+                            System.out.println("Dodałeś do koszyka myszke SteelSeries.");
                             Koszyk.koszyk.add("Myszka SteelSeries");
                             rekomendowane_produkty();
 
                             break;
                         case 3:
-                            System.out.println("Myszka Logitech");
-                            System.out.println("Kupiłeś myszke Myszka Logitech");
+                            System.out.println("Myszka Logitech G502");
+                            System.out.println("Dodałeś do koszyka myszke Myszka Logitech");
                             Koszyk.koszyk.add("Myszka Logitech");
                             rekomendowane_produkty();
 
@@ -449,21 +449,21 @@ public class Main {
                     switch (opcje) {
                         case 1:
                             System.out.println("Komputer ASUS");
-                            System.out.println("Kupiłeś komputer ASUS.");
+                            System.out.println("Dodałeś do koszyka komputer ASUS.");
                             Koszyk.koszyk.add("Komputer Asus");
                             rekomendowane_produkty();
 
                             break;
                         case 2:
                             System.out.println("Komputer Lenovo");
-                            System.out.println("Kupiłeś komputer Lenovo.");
+                            System.out.println("Dodałeś do koszyka komputer Lenovo.");
                             Koszyk.koszyk.add("Komputer Lenovo");
                             rekomendowane_produkty();
 
                             break;
                         case 3:
                             System.out.println("Komputer Dell.");
-                            System.out.println("Kupiłeś komputer Dell.");
+                            System.out.println("Dodałeś do koszyka komputer Dell.");
                             Koszyk.koszyk.add("Komputer Dell");
                             rekomendowane_produkty();
 
@@ -477,21 +477,21 @@ public class Main {
                     switch (opcje) {
                         case 1:
                             System.out.println("Krzesło Diablo");
-                            System.out.println("Kupiłeś krzesło Diablo.");
+                            System.out.println("Dodałeś do koszyka krzesło Diablo.");
                             Koszyk.koszyk.add("Krzesło Diablo");
                             rekomendowane_produkty();
 
                             break;
                         case 2:
                             System.out.println("Krzesło Gamvis");
-                            System.out.println("Kupiłeś krzesło Gamvis.");
+                            System.out.println("Dodałeś do koszyka krzesło Gamvis.");
                             Koszyk.koszyk.add("Krzesło Gamvis");
                             rekomendowane_produkty();
 
                             break;
                         case 3:
                             System.out.println("Krzesło Huzaro.");
-                            System.out.println("Kupiłeś krzesło Huzaro.");
+                            System.out.println("Dodałeś do koszyka krzesło Huzaro.");
                             Koszyk.koszyk.add("Krzesło Huzaro");
                             rekomendowane_produkty();
 
@@ -512,4 +512,4 @@ public class Main {
 
 
     }
-    }
+}
